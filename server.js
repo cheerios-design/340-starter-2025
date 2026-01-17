80dc68d035c3e389dc8d7bc198dad6e815cb1e80
Sam Daramroei <samdotmc@gmail.com>
2026-01-17 17:49:51 +0300

    Add Express routes implementation with index view and missing dependencies

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
+ * Routes
+ *************************/
+app.use(static)
+
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+
+/* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
