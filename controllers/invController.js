f0c10f1c924f05596882d323d81d80f5df5ea1b0
Sam Daramroei <31776791+cheerios-design@users.noreply.github.com>
2026-02-02 11:10:22 +0300

    Assignment 3: Implement inventory item detail view and error handling; add styles for vehicle detail page

--- controllers/invController.js
+++ controllers/invController.js
@@ -19,4 +19,20 @@ invCont.buildByClassificationId = async function (req, res, next) {
   })
 }
 
+/* ***************************
+ *  Build inventory item detail view
+ * ************************** */
+invCont.buildByInventoryId = async function (req, res, next) {
+  const inv_id = req.params.inv_id
+  const data = await invModel.getInventoryByInventoryId(inv_id)
+  const detail = utilities.buildVehicleDetail(data)
+  let nav = await utilities.getNav()
+  const vehicleName = `${data.inv_year} ${data.inv_make} ${data.inv_model}`
+  res.render("./inventory/detail", {
+    title: vehicleName,
+    nav,
+    detail,
+  })
+}
+
 module.exports = invCont
--- utilities/index.js
+++ utilities/index.js
@@ -57,4 +57,33 @@ Util.buildClassificationGrid = async function(data){
   return grid
 }
 
+/* **************************************
+* Build the vehicle detail view HTML
+* ************************************ */
+Util.buildVehicleDetail = function(vehicle){
+  let detail = '<div id="vehicle-detail">'
+  detail += '<div class="vehicle-image">'
+  detail += '<img src="' + vehicle.inv_image + '" alt="Image of ' 
+  + vehicle.inv_make + ' ' + vehicle.inv_model + '">'
+  detail += '</div>'
+  detail += '<div class="vehicle-info">'
+  detail += '<h2>' + vehicle.inv_make + ' ' + vehicle.inv_model + ' Details</h2>'
+  detail += '<p class="vehicle-price"><strong>Price: $' 
+  + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</strong></p>'
+  detail += '<p><strong>Description:</strong> ' + vehicle.inv_description + '</p>'
+  detail += '<p><strong>Color:</strong> ' + vehicle.inv_color + '</p>'
+  detail += '<p><strong>Mileage:</strong> ' 
+  + new Intl.NumberFormat('en-US').format(vehicle.inv_miles) + ' miles</p>'
+  detail += '</div>'
+  detail += '</div>'
+  return detail
+}
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
 module.exports = Util
