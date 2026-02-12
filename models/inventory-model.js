5616bc5426fcac86c2c65a68d23bbf780550591a
Sam Daramroei <31776791+cheerios-design@users.noreply.github.com>
2026-02-12 12:32:41 +0300

    Team Activity - Deleting Data Add delete inventory functionality with confirmation view

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -105,4 +105,17 @@ async function updateInventory(
   }
 }
 
-module.exports = {getClassifications, getInventoryByClassificationId, getInventoryByInventoryId, addClassification, addInventory, updateInventory}
+/* ***************************
+ *  Delete Inventory Item
+ * ************************** */
+async function deleteInventoryItem(inv_id) {
+  try {
+    const sql = 'DELETE FROM inventory WHERE inv_id = $1'
+    const data = await pool.query(sql, [inv_id])
+    return data
+  } catch (error) {
+    new Error("Delete Inventory Error")
+  }
+}
+
+module.exports = {getClassifications, getInventoryByClassificationId, getInventoryByInventoryId, addClassification, addInventory, updateInventory, deleteInventoryItem}
